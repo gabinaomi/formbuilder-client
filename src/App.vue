9971b12a549dd43fcd9a4f6a23b78361977ddc69
gabinaomi <gkamimoto@gmail.com>
2024-09-21 12:20:35 -0300

    disable babel, install mdi icons, instantiate vuetify and developed wireframe in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app id="app">
+    <v-system-bar>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Construtor de Formulários | FormBuilder</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="bg-grey-lighten-2">
+      <v-container> </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "AppView",
+  data: () => ({ drawer: null }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
+  theme: { dark: false },
+  icons: {
+    iconfont: "mdi", // Especifica o uso do MDI como a biblioteca de ícones
+  },
+});
+
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
